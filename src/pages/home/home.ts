e32e3a659b5ed78b53080ddb1a67f28f80f22932
Henrique Cavalcante <henrique.cavalcante@yahoo.com.br>
2019-04-26 07:52:23 -0400

    Projeto de Exemplo - Teste de Debug via Google Chrome e Smartphone

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
+  constructor(public navCtrl: NavController) {}
 
-  constructor(public navCtrl: NavController) {
+  executaComando() {
+    let res = this.soma();
 
+    console.log(`Valor de soma: ${res}`);
   }
 
+  soma(): number {
+    let valor = 0;
+    valor += 1;
+    valor += 10;
+
+    return valor;
+  }
 }
